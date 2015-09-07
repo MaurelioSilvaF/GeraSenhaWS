0a237745a681dcb441acc02b14333de98bf40472
MaurelioSilvaF <maureliosilvaf@gmail.com>
2015-09-07 16:55:20 -0300

    JSPs

--- WebContent/js/script.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-    $.ajax({
-        url: "http://localhost:8080/GeraSenhaWS/api/senhasAtuais"
-    }).then(function(data) 
-       $('col-md-1').append(data);
-    });
-});
